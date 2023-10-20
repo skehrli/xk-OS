315c350dec4104845b28da9a6a55ff850570df9a
Daniel Fang <dyfang@attu1.cs.washington.edu>
2023-10-20 15:43:49 -0700

    test: lab2 part1 passing

--- user/lab2test.c
+++ user/lab2test.c
@@ -30,6 +30,8 @@ int main() {
   dup(0); // stdout
   dup(0); // stderr
 
+  printf(stdout, "lab2test main\n");
+
   pid = fork();
   if (pid < 0) {
     error("fork failed");
@@ -58,6 +60,7 @@ int main() {
 
 void run_test(char* test) {
   if (strcmp(test, "part1\n") == 0) {
+    printf(stdout, "lab2test part1\n");
     fork_wait_exit_basic();
     fork_wait_exit_cleanup();
     fork_wait_exit_multiple();
